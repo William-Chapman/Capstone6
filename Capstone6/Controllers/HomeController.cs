c7dfa70fe444897b126fb0e8e36529de17c436bc
William <chapman.williamt@gmail.com>
2018-08-29 08:56:35 -0400

    git init

---
+++ Capstone6/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Capstone6.Models;
+
+namespace Capstone6.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TaskList()
+        {
+            week6capstone_tasklistEntities ORM = new week6capstone_tasklistEntities();
+            ViewBag.Tasks = ORM.tasks.ToList();
+            return View();
+        }
+
+        public ActionResult AddTask()
+        {
+            return View();
+        }
+
+        public ActionResult SaveTask(task newTask)
+        {
+            week6capstone_tasklistEntities ORM = new week6capstone_tasklistEntities();
+            newTask.userid = 1;
+            ORM.tasks.Add(newTask);
+            ORM.SaveChanges();
+             return RedirectToAction("TaskList");
+        }
+
+        public ActionResult EditTask(int taskid)
+        {
+            week6capstone_tasklistEntities ORM = new week6capstone_tasklistEntities();
+            task taskToEdit = ORM.tasks.Find(taskid);
+            if(taskToEdit == null)
+            {
+                return RedirectToAction("TaskList");
+            }
+            ViewBag.TaskToEdit = taskToEdit;
+            return View();
+        }
+
+        public ActionResult DeleteTask(int taskid)
+        {
+            week6capstone_tasklistEntities ORM = new week6capstone_tasklistEntities();
+            task taskToDelete = ORM.tasks.Find(taskid);
+            ORM.tasks.Remove(taskToDelete);
+            ORM.SaveChanges();
+            return RedirectToAction("TaskList");
+        }
+    }
+}
